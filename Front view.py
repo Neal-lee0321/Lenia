a26f58e4edb655fb3f1caccd23c7ce9d7f7dd9c6
Neal-lee0321 <75286534+Neal-lee0321@users.noreply.github.com>
2023-12-06 14:50:01 +0800

    Update Front view.py

--- Front view.py
+++ Front view.py
@@ -2,91 +2,54 @@ import sys
 import random
 import json
 import os
+import time
+import single_channel as SC
+import numpy as np
+from single_channel import Automaton as Automaton
+from single_channel import Board as Board
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QGraphicsView, QGraphicsScene,
     QGraphicsRectItem, QPushButton, QVBoxLayout, QHBoxLayout, QWidget,
     QAction
 )
 from PyQt5.QtCore import Qt, QTimer
-
-# 0=b=.  1=o=A  1-24=A-X  25-48=pA-pX  49-72=qA-qX  241-255=yA-yO
-def expand(coding):
-    s = ""
-    num = 1
-    i = 0
-    while i < len(coding):
-        if coding[i].isdigit():
-            j = i
-            while coding[j].isdigit():
-                j += 1
-            num = int(coding[i:j])
-            i = j-1
-        else:
-            cur = coding[i]
-            if cur.islower() and cur != 'b' and cur != 'o':
-                i += 1
-                cur += coding[i]
-            for _ in range(num):
-                s += cur
-            num = 1
-        i += 1
-    return s
-
-def decode(string):
-    string = expand(string)
-    cells = [[]]
-    i = 0
-    while i < len(string)-1:
-        c = string[i]
-        i += 1
-        if(c == '$'):
-            cells.append([])
-            continue
-        num = 0
-        if(c == '0' or c == 'b' or c == '.'):
-            num = 0
-        if(c == '1' or c == 'o'):
-            num = 1
-        if(c.isupper()):
-            num = ord(c) - ord('A') + 1
-        if(c.islower() and ord(c) >= ord('p')):
-            num = (ord(c) - ord('p') + 1) * 24 + (ord(string[i]) - ord('A') + 1)
-            i += 1
-        cells[-1].append(num)
-
-    mx = 0
-    for lst in cells:
-        mx = max(mx, len(lst))
-    for lst in cells:
-        while(len(lst) < mx):
-            lst.append(0)
-    return cells
+from PyQt5.QtGui import QColor, QPen
 
 
-class LifeGame(QMainWindow):
+L = 4
+
+class LifeGame(QMainWindow, Automaton, Board):
     def __init__(self):
+        # QMainWindow().__init__(self)
+        # Automaton().__init__(self)
+        # Board().__init__(self)
+        # QGraphicsView().__init__(self)
         super().__init__()
         self.setWindowTitle("Life Game")
-        self.setGeometry(100, 100, 1600, 1200)
+        self.setGeometry(100, 100, 1200, 1200)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
         self.scene = QGraphicsScene()
         self.view = QGraphicsView(self.scene)
+        self.item_index = [[[0, 0]] * SC.SIZEX for i in range(SC.SIZEY)]
+        # self.setViewportUpdateMode(QGraphicsView.NoViewportUpdate) 
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_game)
-        self.timer.start(100)
+        self.timer.start(10)
 
-        self.height = 100
-        self.width = 150
+        self.height = SC.SIZEX
+        self.width = SC.SIZEY
         self.cells = [[False] * self.height for i in range(self.width)]
 
         self.setMouseTracking(True)
         self.draw_state = False
 
+        self.init_lenia()
         self.init_ui()
+
 
     def init_ui(self):
         hbox = QHBoxLayout()
@@ -95,6 +58,7 @@ class LifeGame(QMainWindow):
 
         self.central_widget.setLayout(vbox)
 
+        self.get_color()
         self.random_start()
 
         self.start_button = QPushButton("Stop")
@@ -115,6 +79,12 @@ class LifeGame(QMainWindow):
         menubar = self.menuBar()
         pattern_menu = menubar.addMenu("Patterns")
         self.create_pattern_menu(pattern_menu)
+
+    def init_lenia(self):
+        self.lenia = Automaton()
+        self.lenia.world.cells = np.zeros((SC.SIZEX, SC.SIZEY))
+        self.lenia.calc_kernel()
+
 
 
     def create_pattern_menu(self, menu):
@@ -152,23 +122,58 @@ class LifeGame(QMainWindow):
             location += 1
 
 
+    def get_color(self):
+        # purple (58, 8, 62)
+        # yellow (255, 255, 85)
+        purple = [69, 13, 84]
+        blue = [45, 112, 142]
+        green = [86, 198, 103]
+        yellow = [253, 231, 37]
+        self.color = []
+
+        for val in range(256):
+            state = val / 255.0
+            state = 1-state
+            clr = [0, 0, 0]
+            for i in range(3):
+                if(state < 1/4):
+                    clr[i] = 4 * ((1/4 - state) * yellow[i] + state * green[i])
+                elif(state < 2/3):
+                    clr[i] = 12/5 * ((2/3 - state) * green[i] + (state - 1/4) * blue[i])
+                else:
+                    clr[i] = 3 * ((1 - state) * blue[i] + (state - 2/3) * purple[i])
+                clr[i] = int(clr[i])
+            self.color.append(clr)
+
+
     def add_cell(self, x, y, state):
-        rect = QGraphicsRectItem(0, 0, 10, 10)
-        rect.setPos(10 * x, 10 * y)
-        rect.setBrush(Qt.black if state else Qt.white)
+        rect = QGraphicsRectItem(0, 0, L, L)
+        rect.setPen(QPen(Qt.NoPen))
+        rect.setPos(L * x, L * y)
+        # rect.setBrush(Qt.black if state else Qt.white)
+
+        clr = self.color[int(state * 255)]
+        rect.setBrush(QColor(clr[0], clr[1], clr[2]))
         self.scene.addItem(rect)
+        self.item_index[x][y] = [rect, clr]
 
     def clear_scene(self):
+        for item in self.scene.items():
+            self.scene.removeItem(item)
         for x in range(self.width):
             for y in range(self.height):
-                self.cells[x][y] = False
-                self.add_cell(x, y, False)
+                self.lenia.world.cells[x][y] = 0
+                self.add_cell(x, y, 0)
+        self.scene.update()
 
     def random_start(self):
+        for item in self.scene.items():
+            self.scene.removeItem(item)
         for x in range(self.width):
             for y in range(self.height):
-                self.cells[x][y] = random.choice([True, False])
-                self.add_cell(x, y, self.cells[x][y])
+                self.lenia.world.cells[x][y] = random.uniform(0, 1)
+                self.add_cell(x, y, self.lenia.world.cells[x][y])
+        self.scene.update()
 
     def toggle_game(self):
         if(not self.timer.isActive()):
@@ -183,78 +188,106 @@ class LifeGame(QMainWindow):
         0
     def paint_animal(self, location):
         print("location: ", location)
-        string = self.animal_data[location]["cells"]
-        print(string)
-        self.current_pattern = decode(string)
-        print(self.current_pattern)
+        # string = self.animal_data[location]["cells"]
+        # print(string)
+        # self.current_pattern = decode(string)
+        # print(self.current_pattern)
+        self.current_data = self.animal_data[location]
         self.draw_state = True
+
+        self.lenia = Automaton(Board.from_data(self.current_data))
+        self.lenia.world.cells = np.zeros((SC.SIZEX, SC.SIZEY))
+        self.calc_kernel()
 
 
     def mousePressEvent(self, event):
         pos = self.view.mapToScene(event.pos())
-        x = int((pos.x() - 15) // 10)
-        y = int((pos.y() - 44) // 10)
+        x = int((pos.x() - 15) // L)
+        y = int((pos.y() - 44) // L)
         print("[", pos.x(), ",", pos.y(), "]")
         print("pos_x: ", x, " pos_y: ", y)
 
+        # if(0 <= x < self.width and 0 <= y < self.height):
+        #     side = event.button() == Qt.LeftButton
+        #     n = m = 1
+        #     if(side and self.draw_state == True):
+        #         n = len(self.current_pattern)
+        #         m = len(self.current_pattern[0])
+        #         x = (x - (n-1)/2 + self.width) % self.width
+        #         y = (y - (m-1)/2 + self.height) % self.height
+
+        #         # self.lenia.world.add(SC.Board.from_data({"cells":self.current_pattern / 256}),[x,y])
+        #         print("n:", n), print("m:", m), print(self.current_pattern)
+        #     else:
+        #         self.current_pattern = [[side]]
+            
+        #     for item in self.scene.items():
+        #         if isinstance(item, QGraphicsRectItem):
+        #             tx = int(item.x() / 10)
+        #             ty = int(item.y() / 10)
+        #             px = int((tx - x + self.width) % self.width)
+        #             py = int((ty - y + self.height) % self.height)
+        #             if(px >= 0 and px < n and py >= 0 and py < m):
+        #                 self.cells[tx][ty] = self.current_pattern[px][py]
+        #                 # item.setBrush(Qt.black if self.cells[tx][ty] else Qt.white)
+        #                 clr = 255*(1-self.cells[tx][ty])
+        #                 item.setBrush(QColor(clr, clr, clr))
+        # self.draw_state = False
+
         if(0 <= x < self.width and 0 <= y < self.height):
             side = event.button() == Qt.LeftButton
-            n = m = 1
             if(side and self.draw_state == True):
-                n = len(self.current_pattern)
-                m = len(self.current_pattern[0])
-                x = (x - (n-1)/2 + self.width) % self.width
-                y = (y - (m-1)/2 + self.height) % self.height
-                print("n:", n), print("m:", m), print(self.current_pattern)
-            else:
-                self.current_pattern = [[side]]
-            
-            for item in self.scene.items():
-                if isinstance(item, QGraphicsRectItem):
-                    tx = int(item.x() / 10)
-                    ty = int(item.y() / 10)
-                    px = int((tx - x + self.width) % self.width)
-                    py = int((ty - y + self.height) % self.height)
-                    if(px >= 0 and px < n and py >= 0 and py < m):
-                        self.cells[tx][ty] = self.current_pattern[px][py]
-                        item.setBrush(Qt.black if self.cells[tx][ty] else Qt.white)
-        self.draw_state = False
+                x = (x + self.width) % self.width
+                y = (y + self.height) % self.height
+                x -= self.width//2
+                y -= self.height//2
+                
+                self.lenia.world.add(Board.from_data(self.current_data) ,[int(x), int(y)])
+                print(Board.from_data(self.current_data).cells.shape)
+        
+                for item in self.scene.items():
+                    self.scene.removeItem(item)
+
+                for x in range(self.width):
+                    for y in range(self.height):
+                        self.add_cell(x, y, self.lenia.world.cells[x][y])
+                self.scene.update()
+                
     
 
     def update_game(self):
-        new_cells = [[False] * self.height for i in range(self.width)]
+        start_time = time.time()
+        self.lenia.calc_once()
+        mid_time = time.time()
 
         for x in range(self.width):
             for y in range(self.height):
-                cell = self.cells[x][y]
-                live_neighbors = 0
-                for i in [-1, 0, 1]:
-                    for j in [-1, 0, 1]:
-                        if i == 0 and j == 0:
-                            continue
+                # color = self.get_color(self.lenia.world.cells[x][y])
+                state = self.lenia.world.cells[x][y]
+                c = int((1-state) * 256)
+                clr = self.color[int(state * 255)]
+                # clr = [c, c, c]
+                cur = self.item_index[x][y]
+                if cur[1] != clr:
+                    cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
+                    cur[1] = clr
+        # num = 0
+        # for item in self.scene.items():
+        #     x = int(item.x() / L)
+        #     y = int(item.y() / L)
+        #     color = self.get_color(self.lenia.world.cells[x][y])
+        #     if item.brush().color() != color:
+        #         item.setBrush(color)
+        #         num += 1
+        end_time = time.time()
+        self.scene.update()
+        print(int((mid_time - start_time)*1000), "ms ", int((time.time() - mid_time)*1000), "ms ", 
+              int((time.time() - end_time)*1000), "ms ", len(self.scene.items()))
 
-                        neighbor_x = (x + i + self.width) % self.width
-                        neighbor_y = (y + j + self.height) % self.height
-                        # if(neighbor_x < 0 or neighbor_x >= self.width or neighbor_y < 0 or neighbor_y >= self.height):
-                            # continue
-                        live_neighbors += self.cells[neighbor_x][neighbor_y]
 
-                if(cell):
-                    if live_neighbors < 2 or live_neighbors > 3:
-                        cell = False
-                else:
-                    if live_neighbors == 3:
-                        cell = True
-                new_cells[x][y] = cell
-
-        self.cells = new_cells
-
-        for item in self.scene.items():
-            self.scene.removeItem(item)
-
-        for x in range(self.width):
-            for y in range(self.height):
-                self.add_cell(x, y, self.cells[x][y])
+        # for x in range(self.width):
+        #     for y in range(self.height):
+        #         self.add_cell(x, y, self.lenia.world.cells[x][y])
 
 
 if __name__ == '__main__':
