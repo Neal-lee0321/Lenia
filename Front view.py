aee931ca2abfdb23d80fa49bb0eaf179cd724c65
Neal-lee0321 <75286534+Neal-lee0321@users.noreply.github.com>
2023-12-25 22:43:02 +0800

    Update Front view.py

--- Front view.py
+++ Front view.py
@@ -5,7 +5,10 @@ import os
 import time
 import threading
 import single_channel as SC
+import rebuild as RE
 import numpy as np
+import jax
+import jax.numpy as jnp
 from fractions import Fraction
 from single_channel import Automaton as Automaton
 from single_channel import Board as Board
@@ -20,13 +23,21 @@ from PyQt5.QtGui import QColor, QPen, QIcon
 
 L = 4  # The width of a single cell by pixels 
 D = 80  # The width of the painter by cells
-HERE = os.path.dirname(__file__)
+E = 90  # The width of a new creature in flow lenia
+HERE = os.path.dirname(os.path.realpath(__file__))
 
-class LifeGame(QMainWindow, Automaton, Board):
+def resource_path(relative_path):
+    base_path = getattr(
+        sys, '_MEIPASS',
+        os.path.dirname(os.path.abspath(__file__))
+    )
+    return os.path.join(base_path, relative_path)
+
+class LifeGame(QMainWindow, SC.Automaton, SC.Board, RE.Automaton, RE.Board):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Lenia")
-        self.setWindowIcon(QIcon(os.path.join(HERE, "orbium.png")))
+        self.setWindowIcon(QIcon(resource_path("orbium.png")))
         self.setGeometry(100, 100, 1200, 1200)
 
         self.central_widget = QWidget()
@@ -51,6 +62,7 @@ class LifeGame(QMainWindow, Automaton, Board):
 
         self.init_lenia()
         self.init_ui()
+        self.mode = "Lenia"
 
 
     def init_ui(self):
@@ -95,13 +107,16 @@ class LifeGame(QMainWindow, Automaton, Board):
         scale_menu = menubar.addMenu("Scale")
         self.create_scale_menu(scale_menu)
 
-        kernel_core_menu = menubar.addMenu("Kernel_Core")
+        kernel_core_menu = menubar.addMenu("Kernel-Core")
         self.create_kernel_core_menu(kernel_core_menu)
 
-        field_function_menu = menubar.addMenu("Field_Function")
+        field_function_menu = menubar.addMenu("Field-Function")
         self.create_field_function_menu(field_function_menu)
 
         self.create_toolbar()
+
+        flow_lenia_menu = menubar.addMenu("Flow-Lenia")
+        self.create_flow_lenia_menu(flow_lenia_menu)
 
 
     def tool_make_action(self, param):
@@ -114,7 +129,8 @@ class LifeGame(QMainWindow, Automaton, Board):
                         self.lenia.world.params[param] = value                       
                 elif param in ["R", "T"]:
                     value = int(text)
-                    self.lenia.world.params[param] = value
+                    if value >= 1:
+                        self.lenia.world.params[param] = value
                 elif param == "b":
                     self.lenia.world.params[param] = [Fraction(val) for val in text.split(",")]
                 self.lenia.calc_kernel()
@@ -181,10 +197,73 @@ class LifeGame(QMainWindow, Automaton, Board):
     
 
     def init_lenia(self):
-        self.lenia = Automaton()
+        self.mode = "Lenia"
+        self.lenia = SC.Automaton()
         self.lenia.world.cells = np.zeros((SC.SIZEX, SC.SIZEY))
         self.lenia.calc_kernel()
 
+    def init_flow_lenia(self):
+        self.mode = "Flow Lenia"
+        NBK = 8
+        self.flow_lenia = RE.Automaton(RE.Board([SC.SIZEX, SC.SIZEY], 2), NBK)
+        self.flow_lenia.is_flow = True
+        self.flow_lenia.world.channels = np.zeros((2, SC.SIZEX, SC.SIZEY))
+        self.flow_lenia.nb_k = NBK
+        self.flow_lenia.world.P = jnp.full((NBK, SC.SIZEX, SC.SIZEY), 0)
+
+
+        # 80 [0.75, 0.5, 0.75]
+        # 30 [0.25, 1.0]
+        # 10 [0.75, 0.25]
+
+        # 27 [0.5, 1.0, 0.5]
+        # 17 [0.75]
+        # 24 [1.0, 1.0, 0.75]
+        kernels = []
+        # R = [80, 40, 20, 10]
+        for _ in range(self.flow_lenia.nb_k):
+            num = random.choice([1,2,3])
+            b = []
+            for i in range(num):
+                b.append((random.choice(range(4))+1)/4)  # i/4
+            R = random.choice(range(26)) + 5  # 5~30
+            # kernels.append(self.flow_lenia.calc_kernel({"R":R, "b":b, "m":m, "s":s, "kn":1}))
+            kernels.append(self.flow_lenia.calc_kernel({"R":R, "b":b, "kn":1}))
+            print(R, b)
+        # s = random.uniform(0, 0.1)
+        # m = random.uniform(0.1, 0.5)
+        s = 0.05
+        m = 0.65  
+        self.flow_lenia.world.params = {"s":s, "m":m, "fK":np.array(kernels), "gn":1}
+
+        self.flow_lenia.step_fn = self.flow_lenia.build_system(
+            is_flow = True, SX = SC.SIZEX, SY = SC.SIZEY,
+            nb_k = 8, C = 2, dd = 5, dt = 0.1, sigma = 0.6, theta_A = 2,
+            c0 = [0, 0, 0, 1, 0, 1, 1, 1], c1 = [[0, 2, 6, 7, 4], [1, 3, 5]], n = 1.2
+        )
+
+        for x in range(SC.SIZEX):
+            for y in range(SC.SIZEY):
+                cur = self.item_index[x][y]
+                cur[0].setBrush(QColor(0, 0, 0))
+                cur[1] = [0, 0, 0]
+
+        # self.flow_lenia.world.add(RE.Board.from_data({"code":"4H3t","name":"Helicium serratus turbulentus","cname":"鋸卷虫(湍)","params":{"R":18,"T":10,"b":"1/4,1/2,3/4,1","m":0.2,"s":0.02,"kn":1,"gn":1},"cells":"53.J$42.pN8.GLG$42.qJ8.pIG.T$13.2LG26.E3.sC4.G2.pN$12.JQOLQpDpIpDL20.V3.LsP7.pPpUpXV$11.LpDVOVpN2pXpPpAJ19.OQ2.qJpU.2BO3.pSqWrLrG$9.LpUqMqJqCqEqMqTqOqEpP2pNpFTLE15.EQpPrGrQ2pDQsKsF3.qCsCrLrS$7.EpKqMqOqJqMqRrArGrNrIqJqEqMqOqMpUpAJB12.ELQpDqHrIrVpUpIpXvMsHBE.qJsFqCrSV$6.EpAqH2rGrDrIrNrQrGrNrVrNrLrNrLrDqJpNVLG7.E2QpFpNpPpXqEqJqOrG2rVtQuCqC2pXpUpNpAOsCrQO$6.BVqMrSrVrSsCsWtGsUsHsKsPtBsUsCrIqWqMqCpNpFpDrGsHtOsW2sHrSrNrDqWqO2qM2qHqErDsRuNtJqCpNpPqHqOqCpKpDsPtJsHqH$6.JqErVrQrIsCtBtVuCtOtBtGtQtVtQsWsC2rVrStJvJxMyIxRwQvEuFtJsPrXrIqTqJqEpNpIpPpNqEqOqRqEpApNrQxRtGpUpNpItJuCuFsW$5.OqOrLpXTpIqRrLrXtGuKuIuPuXuNuItQ3tGxC3yOwQtTrVqHpNpDpA2pDpAVOGJOJE.OpXqMrSwSyLqOqC2pXuNuXuPtV$4.pAqWrApULOqErVsCrXtDuXvTwIwDvJuSuNuPuU3yOyDsPpF17.JqHsMtJsCpDEGTpXuI2uCuI$3.TqRrLrSrIqOrLuAvRwA2vOwDwQwXwSwDvOvRwD3yOxMqM19.OpPqHpP4.BLpKqEtOvBpS$2.JpXrArS2sRrNsMvBwQ2xEwVxE2xPxE2wQxC4yOqJ18.BJTpPqEqC2.qCtBLJLpKuAwDtTqW$.BpApXqTrVsMqHLpKrQsPuAwSyByIyOyGxHwVxJ4yOsFE17.EVpFpKqEqTrAqOrX2wVJ.EpKuP2wAtT$.EVqErNrSpA3.qEqJrQvW4yOxRxEyI3yOxMqE15.pA.EpDpSqEqJqWrSsFsPrXsMpK3.pPuPvBvTuN$2.VrIrSpA3.rX2vTwDyL4yOyLyB4yOuST15.qTpUpIpUqOrGrLrVsPtBsUV5.pDsWtJvOtQ$2.qCsMrApKG.qTwX14yOtO16.sCsKqRqWrNsFsKsRtJtTtGQ.2L4.qJvWsK$.GrXsKrVrXqTOqEuPvJuPwNyL2yOyL7yOrG16.tDuSsMsCsMtDtJtLuFuPuK2sR2wIG3.pUvOsWpI$.qEsPsMsKrXQ3.QpNtBxRyDyBxUxW2yDyByI2yO17.uFwIuItLtQuFuIuFuXvOvR2vGxHuP4.rNuNuFrXO$BrGsWtBsHpI4.qTrVuPxMxRxEwVwXxEwXwVxMyIxR17.vMxJvTuPuS2vGuXvO2wIuIrDqH4.QsKtGuAsM$LqJsPtVrG4.uUxUwVwSxExCwIvWwAwDvTvRwNxWwQ17.wLyBwVvWwAwIwFvWwIwXwQsUJ5.rAsFsMtOrD$.LsHuNpN3.qCxCxHuSvE2vWvMuX2vBuPuKvGxEvM17.xMyIxUxCxExMxEwXxJxUxMvJsKtBtTpP.BqJrAsHtJpA$2.tJuCqRpS3.pPpIEqOuUvBuP3uAtOtGtOvOuI17.yL2yOyDyGyLyBxUyB2yLyBxRyOyGrL3.pPsWrVE$2.uSuAtBsRE5.qCuIuNuAtD2tBsPrXrVtOtD16.pU7yO2yL2yOwXvTwFtL4.rDsRqTT$.TvEuUuFsR4.qRsFsKuAtVtJsM2sCrNqWqJrIsC16.sH5yOyL4yOyDtDqJpX4.qTsFrXqWpK$.pKuNvWvGrG3.qHxRwDrXsMsRsMrSrGrDqOpXpFpDqOO14.TtG4yOxWxR5yOuSsHrQpP2.rDsRsCrSqWQ$.JrIuXwIpP3.qRuApU.L2rQrAqJqEpSpDE.LJ14.qEvW4yOxPxU5yOxWxMwVuF2qW2sPsCrSpX$3.sRwNrDpAE6.rArDqMpPpFpAE17.JsC4yOxJxCxPyLyOyIxUxJwXxEwLtOqHpNqOrLrVqWE$3.sHwDuKuUqRE3.EqErXrApXpAJB18.qMxR3yOxCwIwFwXxRxWxMwIuF2sPrQpA2.pNrLrGQ$3.sPvWvMvGrGpKQVuCxPuIrIqEpFL19.qTxC3yOwAvOvJvTwL2wSwLvWtOrGqRpSLEpUrSrAV$3.rSvEvGuSqHpXpUqHxUwIQ.BTG17.pKuC4yOuPuKuFuNvGvTvWvJuUvGvEuItLsFrGrSsMrDpFG$3.pKsPuFuNqEqCqE2qHOB.GrAqTpSJL2JQTpDpKpPpNpIpNpXqTrVtQxC3yOwItB2tDtGtTuNvBuSuFuAuIuSuNtLsPsKsPrSqEpAG$5.rVtQpPqCqJqEpKVpFqMtQuSsWqTpApFpIpSqEqCqJrArLsCsPtJuFvJwFxHxWwXvJrX2rLrQsCsPtDtOtTtOtBsFrXsMsRsC2rIrQrIqHV$5.pItBrAtBrQpNpPpUsFxEuFqOrGrAqMqOqJ2qO3qMqOqTrGrLsCsRsCrSrDVpDpNpXqEqOrIsF2sK2sFsMrXrGrDqWqMpXqHqWqMpD$6.sWsKtBrD.GJuCwFpU2qHsPsMrNqM2pUpPpFQJB8.EJVpNqCqTrNrQrLrDrGrXsKsFrNrD2qTrApXJ$6.sKsHsCqH2.GrNpPpU2qCtDsMqTpSpFOG14.JpFpPpXqHqMqOqRrDrLrIqO2qEqMqET$6.qHqWrIpU2.G2TQGsRrG.OpXpKL15.BOpAVQVpNqMqOqRqMpUpN2pSV$8.pPpU2.Q3.pPtV3.pUpS18.BEBGVpPqCpSpPpNpPpSpNL$9.pU.qJpS3.VrI3.pX22.G2LJGJQpDVJ$9.pNpFpUpD8.qM3.E$10.pPVL7.pAqH$25.L!"}),[0,0],0)
+        # print(self.flow_lenia.world.channels.sum())
+
+        # channels = RE.jnp_to_np(self.flow_lenia.world.channels)
+        # P = RE.jnp_to_np(self.flow_lenia.world.P)
+        # for x in range(SC.SIZEX):
+        #     for y in range(SC.SIZEY):
+        #         state = min((channels[0][x][y] + channels[1][x][y])/2, 1)
+        #         # print(x, y, abs(self.flow_lenia.change[0][x][y]))
+        #         clr = []
+        #         for i in range(3):
+        #             clr.append(state * P[i][x][y])
+        #             clr[-1] = int(clr[-1] * 255)
+        #         cur = self.item_index[x][y]
+        #         cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
+        #         cur[1] = clr
 
 
     def create_pattern_menu(self, menu):
@@ -193,8 +272,8 @@ class LifeGame(QMainWindow, Automaton, Board):
         menu.addAction(ustc_icon)
 
         # HERE = os.path.dirname(__file__)
-        FILE_PATH = os.path.join(HERE, "animals.json")
-        with open(FILE_PATH, encoding = 'utf-8') as file:
+        # FILE_PATH = os.path.join(HERE, "animals.json")
+        with open(resource_path("animals.json"), encoding = 'utf-8') as file:
             self.animal_data = json.load(file)
         print(len(self.animal_data))
 
@@ -285,6 +364,19 @@ class LifeGame(QMainWindow, Automaton, Board):
         field_group.triggered.connect(on_triggered)
 
 
+    def create_flow_lenia_menu(self, menu):
+        action = QAction("Switch to flow lenia", self, checkable = True)
+        menu.addAction(action)
+
+        def on_triggered(checked):
+            if checked:
+                self.init_flow_lenia()
+            else:
+                self.init_lenia()
+                self.clear_scene()
+        action.triggered.connect(on_triggered)
+
+
     def get_color(self):
         # purple = [69, 13, 84]
         # blue = [45, 112, 142]
@@ -334,12 +426,19 @@ class LifeGame(QMainWindow, Automaton, Board):
     def clear_scene(self):
         clr = self.color[0]
         # clr = [255, 255, 255]
-        for item in self.scene.items():
-            item.setBrush(QColor(clr[0], clr[1], clr[2]))
-        self.lenia.world.cells.fill(0)
+        if self.mode == "Lenia":
+            for item in self.scene.items():
+                item.setBrush(QColor(clr[0], clr[1], clr[2]))
+            self.lenia.world.cells.fill(0)
+        elif self.mode == "Flow Lenia":
+            for item in self.scene.items():
+                item.setBrush(QColor(0, 0, 0))
+            self.flow_lenia.world.channels = np.zeros((2, SC.SIZEX, SC.SIZEY))
         self.scene.update()
 
     def random_start(self):
+        if self.mode == "Flow Lenia":
+            return
         for item in self.scene.items():
             self.scene.removeItem(item)
         for x in range(self.width):
@@ -373,9 +472,9 @@ class LifeGame(QMainWindow, Automaton, Board):
         self.current_data = self.animal_data[location]
         # self.draw_state = True
 
-        board = Board.from_data(self.current_data)
+        board = SC.Board.from_data(self.current_data)
         board.params['R'] *= SC.SCALE
-        self.lenia = Automaton(board)
+        self.lenia = SC.Automaton(board)
         self.lenia.world.cells = np.zeros((SC.SIZEX, SC.SIZEY))
         self.calc_kernel()
         self.clear_scene()
@@ -386,8 +485,8 @@ class LifeGame(QMainWindow, Automaton, Board):
 
     def mousePressEvent(self, event):
         pos = self.view.mapToScene(event.pos())
-        x = int((pos.x() - 15) // L)
-        y = int((pos.y() - 44) // L)
+        x = int((pos.x() - 14) // L)
+        y = int((pos.y() - 76) // L)
         print("[", pos.x(), ",", pos.y(), "]")
         print("pos_x: ", x, " pos_y: ", y)
 
@@ -420,7 +519,7 @@ class LifeGame(QMainWindow, Automaton, Board):
 
         if(0 <= x < self.width and 0 <= y < self.height):
             side = event.button() == Qt.LeftButton
-            if(side):
+            if side and self.mode == "Lenia":
                 x = (x + self.width) % self.width
                 y = (y + self.height) % self.height
                 x -= self.width//2
@@ -440,7 +539,7 @@ class LifeGame(QMainWindow, Automaton, Board):
                         cur[1] = clr
                 # self.scene.update()
             
-            elif side == False:
+            elif side == False and self.mode == "Lenia":
                 x = int((x - D/2 + self.width) % self.width)
                 y = int((y - D/2 + self.height) % self.height)
                 for i in range(D):
@@ -455,18 +554,72 @@ class LifeGame(QMainWindow, Automaton, Board):
                         cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
                         cur[1] = clr
 
+            elif side and self.mode == "Flow Lenia":
+                x = int((x - E/2 + self.width) % self.width)
+                y = int((y - E/2 + self.height) % self.height)
+
+                # kc = random.choice(range(self.flow_lenia.nb_k))
+                gene = [random.uniform(0, 1) for i in range(self.flow_lenia.nb_k)]
+                # gene = [0 for i in range(self.flow_lenia.nb_k)]
+                # gene[kc] = 1
+
+                channels = RE.jnp_to_np(self.flow_lenia.world.channels)
+                P = np.array(jax.device_get(self.flow_lenia.world.P))
+                # print("P.shape ", self.flow_lenia.world.P)
+                # print("numpy shape", P.shape)
+                for i in range(E):
+                    for j in range(E):
+                        px = (x + i + self.width) % self.width
+                        py = (y + j + self.height) % self.height
+                        # r = np.sqrt((i-E/2.0) * (i-E/2.0) + (j-E/2.0) * (j-E/2.0)) / (E/2.0)
+                        # channels[0][px][py] = random.uniform(0.7, 1) * self.flow_lenia.kernel_core[0](r) if r < 1 else 0
+                        # channels[1][px][py] = random.uniform(0.7, 1) * self.flow_lenia.kernel_core[0](r) if r < 1 else 0
+                        channels[0][px][py] = random.uniform(0, 1)
+                        channels[1][px][py] = random.uniform(0, 1)
+                        for _ in range(self.flow_lenia.nb_k):
+                            P[_][px][py] = gene[_]
+                        cur = self.item_index[px][py]
+                        clr = [int(gene[i] * (channels[0][px][py] + channels[1][px][py])/2 * 255) for i in range(self.flow_lenia.nb_k)]
+                        cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
+                        cur[1] = clr
+
+                self.flow_lenia.world.channels = jnp.array(channels)
+                self.flow_lenia.world.P = jnp.array(P)
+
     
 
     def update_game(self):
         start_time = time.time()
-        self.lenia.calc_once()
+        new_P = jnp.array([])
+        new_channels = jnp.array([])
+
+        if self.mode == "Lenia":
+            self.lenia.calc_once()
+
+        elif self.mode == "Flow Lenia":
+            new_channels, new_P = self.flow_lenia.step_fn(
+                self.flow_lenia.world.channels, self.flow_lenia.world.P, self.flow_lenia.world.params)
+            
+            self.flow_lenia.change = np.abs(RE.jnp_to_np(new_channels - self.flow_lenia.world.channels))
+            delta_P = new_P - self.flow_lenia.world.P
+            for i in range(self.flow_lenia.nb_k):
+                self.flow_lenia.change[0] += np.abs(RE.jnp_to_np(delta_P[i]))
+            self.flow_lenia.world.channels = new_channels
+            self.flow_lenia.world.P = new_P
+            # print(np.nonzero(self.flow_lenia.change))
+
+            new_P = jnp.transpose(new_P, (1, 2, 0))
+            new_P = RE.jnp_to_np(new_P)
+            new_channels = RE.jnp_to_np(new_channels)
+
         mid_time = time.time()
 
-        to_update = []
-        non_zero = np.nonzero(self.lenia.change)
 
-        for x, y in zip(non_zero[0], non_zero[1]):
-            if(abs(self.lenia.change[x][y]) > 3/255):
+        non_zero = np.nonzero(self.lenia.change) if self.mode == "Lenia" else np.nonzero(self.flow_lenia.change)
+
+        count = 0
+        for x, y in (zip(non_zero[0], non_zero[1]) if self.mode == "Lenia" else zip(non_zero[1], non_zero[2])):
+            if self.mode == "Lenia" and abs(self.lenia.change[x][y]) > 3/255:
                 state = self.lenia.world.cells[x][y]
                 c = int((1-state) * 255)
                 clr = self.color[int(state * 255)]
@@ -476,13 +629,22 @@ class LifeGame(QMainWindow, Automaton, Board):
                 cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
                 cur[1] = clr
 
+            elif self.mode == "Flow Lenia" and new_channels[0][x][y] + new_channels[1][x][y] > 0.05*2:
+                state = min((new_channels[0][x][y] + new_channels[1][x][y])/2, 1)
+                # print(x, y, abs(self.flow_lenia.change[0][x][y]))
+                clr = []
+                for i in range(3):
+                    clr.append(state * new_P[x][y][i])
+                    clr[-1] = int(clr[-1] * 255)
+                cur = self.item_index[x][y]
+                cur[0].setBrush(QColor(clr[0], clr[1], clr[2]))
+                cur[1] = clr
+                count += 1
+
         end_time = time.time()
 
-        for item, r, g, b in to_update:
-            item.setBrush(QColor(r, g, b))
-        # self.scene.update()
-        print(int((mid_time - start_time)*1000), "ms ", int((time.time() - mid_time)*1000), "ms ", 
-              int((time.time() - end_time)*1000), "ms ", len(self.scene.items()), len(to_update))
+        # print(int((mid_time - start_time)*1000), "ms ", int((time.time() - mid_time)*1000), "ms ", 
+            #   int((time.time() - end_time)*1000), "ms ", len(self.scene.items()), count)
 
 
         # for x in range(self.width):
